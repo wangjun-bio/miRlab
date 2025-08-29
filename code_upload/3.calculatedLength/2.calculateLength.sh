183486efadfeeca65680f27e76d27cfcfe231499
Petunias77 <151997012+Petunias77@users.noreply.github.com>
2025-08-29 19:24:19 +0800

    Delete code_upload directory

--- code_upload/3.calculatedLength/2.calculateLength.sh
+++
@@ -1,9 +0,0 @@
-rm -rf ./length
-mkdir -p length
-rm -rf ./sample.txt
-find . -name "*.fastq" -exec basename {} \; | sed 's/\.fastq$//' > sample.txt
-while IFS= read -r line;
-do
-	echo "${line} is processing"
-	python calculateLength.py -i ./${line}.fastq -o ./length/${line}.tsv -t 60
-done < "sample.txt"
